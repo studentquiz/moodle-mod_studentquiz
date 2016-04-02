10eb8e9fb76676e1d8bbd2b1ab9c2f34eeddcf48
Dena Moshfegh <dmoshfegh@dmo.local>
2016-04-02 14:39:26 +0200

    changed structur of views

--- view.php
+++ view.php
@@ -1,83 +1,88 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+define('CACHE_DISABLE_ALL', true);
+define('CACHE_DISABLE_STORES', true);
 
-/**
- * Prints a particular instance of studentquiz
- *
- * You can have a rather longer description of the file as well,
- * if you like, and it can span multiple lines.
- *
- * @package    mod_studentquiz
- * @copyright  2016 HSR (http://www.hsr.ch)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot . '/question/editlib.php');
 
-// Replace studentquiz with the name of your module and remove this line.
+class TextCustomView extends \core_question\bank\view {
+    public function __construct($contexts, $pageurl, $course, $cm) {
+        parent::__construct($contexts, $pageurl, $course, $cm);
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once(dirname(__FILE__).'/lib.php');
+    }
 
-$id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
-$n  = optional_param('n', 0, PARAM_INT);  // ... studentquiz instance ID - it should be named as the first character of the module.
+    public function display($tabname, $page, $perpage, $cat,
+                               $recurse, $showhidden, $showquestiontext){
+        global $OUTPUT;
 
-if ($id) {
-    $cm         = get_coursemodule_from_id('studentquiz', $id, 0, false, MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $studentquiz  = $DB->get_record('studentquiz', array('id' => $cm->instance), '*', MUST_EXIST);
-} else if ($n) {
-    $studentquiz  = $DB->get_record('studentquiz', array('id' => $n), '*', MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $studentquiz->course), '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('studentquiz', $studentquiz->id, $course->id, false, MUST_EXIST);
-} else {
-    error('You must specify a course_module ID or an instance ID');
+        if ($this->process_actions_needing_ui()) {
+            return;
+        }
+        $editcontexts = $this->contexts->having_one_edit_tab_cap($tabname);
+        // Category selection form.
+        echo $OUTPUT->heading('Student-Quiz - Made by DM, MS', 2);
+        array_unshift($this->searchconditions, new \core_question\bank\search\hidden_condition(!$showhidden));
+        array_unshift($this->searchconditions, new \core_question\bank\search\category_condition(
+            $cat, $recurse, $editcontexts, $this->baseurl, $this->course));
+        $this->display_options_form($showquestiontext);
+
+        // Continues with list of questions.
+        $this->display_question_list($this->contexts->having_one_edit_tab_cap($tabname),
+            $this->baseurl, $cat, $this->cm,
+            null, $page, $perpage, $showhidden, $showquestiontext,
+            $this->contexts->having_cap('moodle/question:add'));
+    }
+
+    /*protected function init_search_conditions() {
+        $searchplugins = get_plugin_list_with_function('mod', 'get_question_bank_search_conditions');
+        foreach ($searchplugins as $component => $function) {
+            foreach ($function($this) as $searchobject) {
+                $this->add_searchcondition($searchobject);
+            }
+        }
+    }*/
 }
 
-require_login($course, true, $cm);
+function mod_studentquiz_get_question_bank_search_conditions() {
+    echo "get extendsion";
+    return array();
+}
 
-$event = \mod_studentquiz\event\course_module_viewed::create(array(
-    'objectid' => $PAGE->cm->instance,
-    'context' => $PAGE->context,
-));
-$event->add_record_snapshot('course', $PAGE->course);
-$event->add_record_snapshot($PAGE->cm->modname, $studentquiz);
-$event->trigger();
+$coursemodule = required_param('id', PARAM_INT);
+$context = context_module::instance($coursemodule);
+$category = question_get_default_category($context->id);
+$_GET['cmid'] = $coursemodule;
+$_POST['cat'] = $category->id . ',' . $context->id;
+list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
+    question_edit_setup('questions', '/question/edit.php', true, false);
 
-// Print the page header.
 
-$PAGE->set_url('/mod/studentquiz/view.php', array('id' => $cm->id));
-$PAGE->set_title(format_string($studentquiz->name));
-$PAGE->set_heading(format_string($course->fullname));
 
-/*
- * Other things you may want to set - remove if not needed.
- * $PAGE->set_cacheable(false);
- * $PAGE->set_focuscontrol('some-html-id');
- * $PAGE->add_body_class('studentquiz-'.$somevar);
- */
+$url = new moodle_url($thispageurl);
+if (($lastchanged = optional_param('lastchanged', 0, PARAM_INT)) !== 0) {
+    $url->param('lastchanged', $lastchanged);
+}
+$PAGE->set_url($url);
 
-// Output starts here.
+$questionbank = new TextCustomView($contexts, $thispageurl, $COURSE, $cm);
+
+$questionbank->process_actions();
+
+// TODO log this page view.
+
+$context = $contexts->lowest();
+$streditingquestions = get_string('editquestions', 'question');
+$PAGE->set_title($streditingquestions);
+$PAGE->set_heading($COURSE->fullname);
+
 echo $OUTPUT->header();
 
-// Conditions to show the intro can change to look for own settings or whatever.
-if ($studentquiz->intro) {
-    echo $OUTPUT->box(format_module_intro('studentquiz', $studentquiz, $cm->id), 'generalbox mod_introbox', 'studentquizintro');
-}
 
-// Replace the following lines with you own code.
-echo $OUTPUT->heading('Yay! It works!');
 
-// Finish the page.
+echo '<div class="questionbankwindow boxwidthwide boxaligncenter">';
+$questionbank->display('questions', $pagevars['qpage'], $pagevars['qperpage'],
+    $pagevars['cat'], $pagevars['recurse'], $pagevars['showhidden'],
+    $pagevars['qbshowtext']);
+echo "</div>\n";
+
 echo $OUTPUT->footer();
