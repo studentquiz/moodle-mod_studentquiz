ffe337abb74c1c5f624a9231de45d0f6b76a69af
Manu <manuel_schuler@hotmail.com>
2016-04-16 17:39:09 +0200

    modify workflow control

--- attempt.php
+++ attempt.php
@@ -8,17 +8,18 @@ $sessionid = required_param('id', PARAM_INT);
 $session = $DB->get_record('studentquiz_practice_session', array('id' => $sessionid));
 
 $cm = get_coursemodule_from_instance('studentquiz', $session->studentquiz_id);
+
 $course = $DB->get_record('course', array('id' => $cm->course));
 
 require_login($course, true, $cm);
 $context = context_module::instance($cm->id);
 
-require_capability('mod/studentquiz:attempt', $context);
+//require_capability('mod/studentquiz:attempt', $context);
 $params = array(
     'objectid' => $cm->id,
     'context' => $context
 );
-$event = \mod_studentquiz\event\studentquiz_attempted::create($params);
+$event = \mod_studentquiz\event\studentquiz_practice_attempted::create($params);
 $event->trigger();
 
 $quba = question_engine::load_questions_usage_by_activity($session->question_usage_id);
@@ -36,14 +37,14 @@ if (data_submitted()) {
         $fraction = $quba->get_question_fraction($slot);
         $maxmarks = $quba->get_question_max_mark($slot);
         $obtainedmarks = $fraction * $maxmarks;
-        $updatesql = "UPDATE {qpractice_session}
-                          SET marksobtained = marksobtained + ?, totalmarks = totalmarks + ?
+        $updatesql = "UPDATE {studentquiz_practice_session}
+                          SET marks_obtained = marks_obtained + ?, total_marks = total_marks + ?
                         WHERE id=?";
         $DB->execute($updatesql, array($obtainedmarks, $maxmarks, $sessionid));
 
         if ($fraction > 0) {
-            $updatesql1 = "UPDATE {qpractice_session}
-                          SET totalnoofquestionsright = totalnoofquestionsright + '1'
+            $updatesql1 = "UPDATE {studentquiz_practice_session}
+                          SET total_no_of_questions_right = total_no_of_questions_right + '1'
                         WHERE id=?";
             $DB->execute($updatesql1, array($sessionid));
         }
@@ -57,7 +58,7 @@ if (data_submitted()) {
             'objectid' => $cm->id,
             'context' => $context
         );
-        $event = \mod_qpractice\event\qpractice_finished::create($params);
+        $event = \mod_studentquiz\event\studentquiz_practice_finished::create($params);
         $event->trigger();
         redirect($stopurl);
     } else {
@@ -108,9 +109,9 @@ $html .= $quba->render_question($slot, $options, $slot);
 // Finish the question form.
 $html .= html_writer::start_tag('div');
 $html .= html_writer::empty_tag('input', array('type' => 'submit',
-    'name' => 'next', 'value' => get_string('nextquestion', 'qpractice')));
+    'name' => 'next', 'value' => get_string('practice_nextquestion', 'studentquiz')));
 $html .= html_writer::empty_tag('input', array('type' => 'submit',
-    'name' => 'finish', 'value' => get_string('stoppractice', 'qpractice')));
+    'name' => 'finish', 'value' => get_string('practice_stoppractice', 'studentquiz')));
 $html .= html_writer::end_tag('div');
 $html .= html_writer::end_tag('form');
 
