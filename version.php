24a88a0abad3ae53ee66dc7291e6e744c77e8a56
Dena Moshfegh <dmoshfegh@dmo.local>
2016-04-18 14:18:46 +0200

    fixed table column name

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2016040100;
+$plugin->version      = 2016040101;
 $plugin->release      = 'v0.0.0';
 $plugin->requires     = 2014051200;
 $plugin->maturity     = MATURITY_ALPHA;
