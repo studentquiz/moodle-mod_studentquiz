9cc3c307baa48a70bac7897a813cf1cbbedb0e03
Dena Moshfegh <dmoshfegh@dmo.local>
2016-06-02 17:27:04 +0200

    update version

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2016052900;
-$plugin->release      = 'v1.0.0';
-$plugin->requires     = 2015111600; // Version 3.0.
+$plugin->version      = 2016052903;
+$plugin->release      = 'v1.0.3';
+$plugin->requires     = 2015051106; // Version 3.0.
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
 $plugin->dependencies = array(
