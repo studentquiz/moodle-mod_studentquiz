e608a6b7bdf9feae6f9b9af5c95927ff80fb1f5d
Lukas Dürrenberge <lduerren@hsr.ch>
2016-11-24 13:38:34 +0100

    Updated version to v1.0.10.

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2016110300;
-$plugin->release      = 'v1.0.9';
+$plugin->version      = 2016112400;
+$plugin->release      = 'v1.0.10';
 $plugin->requires     = 2015102300; // Version 3.0.
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
