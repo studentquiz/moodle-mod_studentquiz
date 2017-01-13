a3fe9e7cbc9a2e75989efe6857c2887ccba9976f
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2017-01-13 18:20:34 +0100

    Fixed version date.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2016122001;
+$plugin->version      = 2017011301;
 $plugin->release      = 'v2.0.0';
 $plugin->requires     = 2015102300; // Version 3.0.
 $plugin->maturity     = MATURITY_STABLE;
