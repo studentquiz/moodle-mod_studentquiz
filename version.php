dc58a5dac31fdfacc4366a9443613e991a993bee
Dionysius <dragon.dionysius@gmail.com>
2018-07-24 12:03:28 +0200

    bump version

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2018051300;
-$plugin->release      = 'v3.0.3';
+$plugin->version      = 2018072400;
+$plugin->release      = 'v3.1.1';
 $plugin->requires     = 2016052300; // Version MOODLE_31, 3.1.0.
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
