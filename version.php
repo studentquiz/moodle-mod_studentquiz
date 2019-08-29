3adbaae1c63b33cf60210f00e1da6e76f2d17a75
Dionysius <dragon.dionysius@gmail.com>
2019-08-29 15:33:56 +0200

    Update version.php

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2019082200;
-$plugin->release      = 'v4.0.3';
+$plugin->version      = 2019082900;
+$plugin->release      = 'v4.0.4';
 $plugin->requires     = 2018051700; // Version MOODLE_35, 3.5.0+.
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
