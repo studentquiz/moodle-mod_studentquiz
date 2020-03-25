ea75d675dd8551b184213632af8730f862c9efbd
Huong Nguyen <huongnv13@gmail.com>
2020-03-25 14:03:49 +0700

    Incorrect comment deletion period helptext

--- settings.php
+++ settings.php
@@ -125,9 +125,12 @@ if ($ADMIN->fulltree) {
         '0'
     ));
 
+    $commentdeletionhelp = str_replace('%%%STUDENTQUIZ_PERIOD_PLACEHOLDER%%%',
+            \mod_studentquiz\commentarea\container::DELETION_PERIOD_DEFAULT,
+            get_string('settings_commentdeletionperiod_help', 'studentquiz'));
     $settings->add(new admin_setting_configselect('studentquiz/commentdeletionperiod',
             get_string('settings_commentdeletionperiod', 'studentquiz'),
-            get_string('settings_commentdeletionperiod_help', 'studentquiz'),
+            $commentdeletionhelp,
             \mod_studentquiz\commentarea\container::DELETION_PERIOD_DEFAULT,
             \mod_studentquiz\commentarea\container::get_deletion_period_options()
     ));
