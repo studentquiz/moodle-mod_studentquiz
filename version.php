be443782cde143dc497ee29f456910baf7372a9f
Dionysius <1341084+dionysius@users.noreply.github.com>
2020-05-24 13:43:25 +0200

    release v4.3.1

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_studentquiz';
-$plugin->version      = 2020052300;
-$plugin->release      = 'v4.3.0';
+$plugin->version      = 2020052400;
+$plugin->release      = 'v4.3.1';
 $plugin->requires     = 2018051700; // Version MOODLE_35, 3.5.0+.
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
