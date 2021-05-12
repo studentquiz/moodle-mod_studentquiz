792296309d2dcf026ba3d3b130148dcabf8d0dea
Denis Manente <denis.manente@automatic-server.com>
2021-05-12 19:07:29 +0200

    fix line length of behat given instruction

--- tests/behat/behat_mod_studentquiz.php
+++ tests/behat/behat_mod_studentquiz.php
@@ -40,7 +40,7 @@ class behat_mod_studentquiz extends behat_base {
     /**
      * Behat function to set availability field
      *
-     * @Given /^I set the availability field "(?P<field_string>(?:[^"]|\\")*)" to "(?P<days_value_integer>(?:[^"]|\\")*)" days from now$/
+     * @Given I set the availability field ":field" to ":days" days from now
      * @param string $field Field name.
      * @param int $days Number of days from now.
      */
@@ -74,7 +74,7 @@ class behat_mod_studentquiz extends behat_base {
     /**
      * Behat function to check moodle branch is greater or equal provided value
      *
-     * @Given /^I make sure the current Moodle branch is greater or equal "(?P<version_value_integer>(?:[^"]|\\")*)"$/
+     * @Given I make sure the current Moodle branch is greater or equal ":version"
      *
      * @param int $version
      */
@@ -89,7 +89,7 @@ class behat_mod_studentquiz extends behat_base {
     /**
      * Enter the text into the editor, this step will trigger trusted event.
      *
-     * @Given /^I enter the text "([^"]*)" into the "([^"]*)" editor/
+     * @Given I enter the text ":value" into the ":fieldlocator" editor
      *
      * @param string $value
      * @param string $fieldlocator
