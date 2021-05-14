81c5a45e237368e04533c18ae48a3393f6521263
Denis Manente <denis.manente@automatic-server.com>
2021-05-14 16:35:19 +0200

    fix moodle code checker for PSR2.Methods.MethodDeclaration.StaticBeforeVisibility error

--- backup/moodle2/restore_studentquiz_activity_task.class.php
+++ backup/moodle2/restore_studentquiz_activity_task.class.php
@@ -58,7 +58,7 @@ class restore_studentquiz_activity_task extends restore_activity_task {
      * Define the contents in the activity that must be
      * processed by the link decoder
      */
-    static public function define_decode_contents() {
+    public static function define_decode_contents() {
         $contents = array();
 
         $contents[] = new restore_decode_content('studentquiz', array('intro'), 'studentquiz');
@@ -70,7 +70,7 @@ class restore_studentquiz_activity_task extends restore_activity_task {
      * Define the decoding rules for links belonging
      * to the activity to be executed by the link decoder
      */
-    static public function define_decode_rules() {
+    public static function define_decode_rules() {
         $rules = array();
 
         $rules[] = new restore_decode_rule('STUDENTQUIZVIEWBYID', '/mod/studentquiz/view.php?id=$1', 'course_module');
@@ -85,7 +85,7 @@ class restore_studentquiz_activity_task extends restore_activity_task {
      * StudentQuiz logs. It must return one array
      * of {@see restore_log_rule} objects
      */
-    static public function define_restore_log_rules() {
+    public static function define_restore_log_rules() {
         $rules = array();
 
         $rules[] = new restore_log_rule('studentquiz', 'add', 'view.php?id={course_module}', '{studentquiz}');
@@ -105,7 +105,7 @@ class restore_studentquiz_activity_task extends restore_activity_task {
      * by the restore final task, but are defined here at
      * activity level. All them are rules not linked to any module instance (cmid = 0)
      */
-    static public function define_restore_log_rules_for_course() {
+    public static function define_restore_log_rules_for_course() {
         $rules = array();
 
         $rules[] = new restore_log_rule('studentquiz', 'view all', 'index.php?id={course}', null);
